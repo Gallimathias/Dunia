c65e9b42e29ca25d87abd42f494db5872a9c8421
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2017-07-15 21:07:11 +0200

    Episode 1 + 2:
    First steps with white ball.
    
    Add properties for position
    Add properties for speed
    Add border check

--- Dunia.Core/GameView.Designer.cs
+++ Dunia.Core/GameView.Designer.cs
@@ -1,13 +1,13 @@
-﻿namespace Dunia.GUI
+﻿namespace Dunia.Core
 {
-    partial class MainForm
+    partial class GameView
     {
-        /// <summary>
+        /// <summary> 
         /// Erforderliche Designervariable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Verwendete Ressourcen bereinigen.
         /// </summary>
         /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
@@ -20,40 +20,27 @@
             base.Dispose(disposing);
         }
 
-        #region Vom Windows Form-Designer generierter Code
+        #region Vom Komponenten-Designer generierter Code
 
-        /// <summary>
-        /// Erforderliche Methode für die Designerunterstützung.
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
         /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
         /// </summary>
         private void InitializeComponent()
         {
-            this.gameView1 = new Dunia.Core.GameView();
             this.SuspendLayout();
             // 
-            // gameView1
-            // 
-            this.gameView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gameView1.Location = new System.Drawing.Point(0, 0);
-            this.gameView1.Name = "gameView1";
-            this.gameView1.Size = new System.Drawing.Size(644, 613);
-            this.gameView1.TabIndex = 0;
-            // 
-            // MainForm
+            // GameView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(644, 613);
-            this.Controls.Add(this.gameView1);
-            this.Name = "MainForm";
-            this.Text = "Dunia";
+            this.DoubleBuffered = true;
+            this.Name = "GameView";
+            this.Size = new System.Drawing.Size(364, 347);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Core.GameView gameView1;
     }
 }
-
---
+++ Dunia.Core/GameView.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dunia.Core
+{
+    public partial class GameView : UserControl
+    {
+        public int Y { get => y; set => y = CheckValue(value, Height, 0); }
+        public int X { get => x; set => x = CheckValue(value, Width, 0); }
+
+        public int SpeedX { get => speedX; set => speedX = CheckValue(value, 30, -30); }
+        public int SpeedY { get => speedY; set => speedY = CheckValue(value, 30, -30); }
+
+        private int x, y;
+
+        Timer timer;
+        int speedX, speedY;
+
+        public GameView()
+        {
+            InitializeComponent();
+            X = Width / 2;
+            Y = Height / 2;
+
+            speedY = 5;
+            speedX = 0;
+
+            BackColor = Color.CornflowerBlue;
+
+            timer = new Timer()
+            {
+                Interval = 20
+            };
+
+            timer.Tick += (s, e) => Invalidate();
+
+            timer.Start();
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+
+            using (var brush = new SolidBrush(Color.White))
+            {
+                e.Graphics.FillEllipse(brush, GetRectangle());
+            }
+
+
+            base.OnPaint(e);
+        }
+
+        private Rectangle GetRectangle()
+        {
+            //if (SpeedY != 0)
+            //    SpeedY -= SpeedY - 2;
+
+            //if (SpeedX != 0)
+            //    SpeedX -= SpeedX - 2;
+
+            int x = X + SpeedX;
+            int y = Y + SpeedY;
+
+            if (y + 100 > Height || y - 100 < 0)
+            {
+                SpeedY *= -1;
+                SpeedX += 5;
+            }
+
+            if (x + 100 > Height || x - 100 < 0)
+            {
+                SpeedX *= -1;
+                SpeedY += 5;
+            }
+
+            X = x;
+            Y = y;
+
+            return new Rectangle(X, Y, 100, 100);
+        }
+
+        private int CheckValue(int value, int max, int min)
+        {
+            if (value + 100 < min)
+                return min - 100;
+
+            if (value + 100 > max)
+                return max - 100;
+
+            return value;
+        }
+    }
+}
